44d0366a4b9eb281a03a82b97071f05433aa53d8
Edna Liliana Galiano Camacho <alu0100762846@ull.edu.es>
2015-10-30 21:11:35 +0000

    Funciones suma y resta implementadas

--- lib/fracciones.rb
+++ lib/fracciones.rb
@@ -21,4 +21,21 @@ class Fraccion
 		"#{@num}/#{@den}"						#Muestra numerador/denominador
 	end
 	
+	def reducir (fraccion)
+		res_max = gcd(fraccion.num, fraccion.den)			#Hallamos el MCD entre el numerador y denominador
+		Fraccion.new(fraccion.num/res_max, fraccion.den/res_max)
+	end
+	
+	def + (fraccion)
+		res_min = mcm(@den, fraccion.den)				#Calculamos el mcm de los denominadores
+		suma = Fraccion.new((res_min/@den*@num)+(res_min/fraccion.den*fraccion.num), res_min) #Fraccion suma
+		reducir (suma)							#Simplificamos el resultado
+	end	
+
+	def - (fraccion)
+		res_min = mcm(@den, fraccion.den)				#Calculamos el mcm de los denominadores
+		resta = Fraccion.new((res_min/@den*@num)-(res_min/fraccion.den*fraccion.num), res_min) 	#Fraccion resta
+		reducir (resta)							#Simplificamos el resultado
+	end	
+	
 end
